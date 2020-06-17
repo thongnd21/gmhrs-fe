ecd1aaf9ce1b4477e64106437340835be9bc0d63
thanh phan <thanhpvse62274@fpt.edu.vn>
2020-06-17 15:23:57 +0700

    update: test connection, save connection string

--- src/app/components/company-config-connection/company-config-connection.component.ts
+++ src/app/components/company-config-connection/company-config-connection.component.ts
@@ -3,6 +3,9 @@ import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@ang
 import { ToastrService } from 'ngx-toastr';
 import { CompanyServices } from '../../api-services/company.services';
 import { AccountCompanyModel } from '../../model/accounts';
+import { CompanyConnection } from '../../model/company_connection';
+import { CompanyConnectionService } from '../../api-services/company-connection-api.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-company-config-connection',
@@ -16,43 +19,45 @@ export class CompanyConfigConnectionComponent implements OnInit {
   account;
   fileContent;
   file;
+  companyConnection;
   controls;
-  days=[
+  days = [
     {
       id: 0,
-      name:'Monday'
+      name: 'Monday'
     },
     {
       id: 1,
-      name:'Monday'
+      name: 'Monday'
     },
     {
       id: 2,
-      name:'Tuesday'
+      name: 'Tuesday'
     },
     {
       id: 3,
-      name:'Wednesday'
+      name: 'Wednesday'
     },
     {
       id: 4,
-      name:'Thursday'
+      name: 'Thursday'
     },
     {
       id: 5,
-      name:'Friday'
+      name: 'Friday'
     },
     {
       id: 6,
-      name:'Saturday'
+      name: 'Saturday'
     },
   ];
   getDaysOfWeek: FormGroup;
   getTime: FormGroup;
-  timeForm : FormGroup;
+  timeForm: FormGroup;
   constructor(
     private toast: ToastrService,
     private companyServices: CompanyServices,
+    private companyConnectionService: CompanyConnectionService,
     private fb: FormBuilder,
   ) {
 
@@ -70,11 +75,12 @@ export class CompanyConfigConnectionComponent implements OnInit {
 
   createAForm() {
     this.accessDBForm = this.fb.group({
-      connectionName: new FormControl('', [Validators.required, Validators.email]),
-      hostname: new FormControl('', [Validators.required]),
+      dbName: new FormControl('', [Validators.required, Validators.email]),
+      host: new FormControl('', [Validators.required]),
       port: new FormControl('', [Validators.required]),
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required]),
+      dialect: new FormControl(''),
     });
 
 
@@ -87,11 +93,11 @@ export class CompanyConfigConnectionComponent implements OnInit {
   }
 
 
-// api endpoint
+  // api endpoint
   onSubmitURLConection(value) {
     this.account = new AccountCompanyModel();
     this.account.id = localStorage.getItem('id');
-    this.account.api_endpoint  =  value.url;
+    this.account.api_endpoint = value.url;
     console.log(this.account);
     this.companyServices.updateAccountCompany(this.account).subscribe(
       (res) => {
@@ -104,16 +110,51 @@ export class CompanyConfigConnectionComponent implements OnInit {
   }
 
 
-  // conection string 
-  onSubmitConection(value) {
-    // let connectionString = value.hostname
-    this.account = new AccountCompanyModel();
-    this.account.id = localStorage.getItem('id');
-    this.account.connection_database  =  JSON.stringify(value);
-    console.log(this.account);
-    this.companyServices.updateAccountCompany(this.account).subscribe(
+  // test connection string
+  onTestConection(value) {
+    this.companyConnection = new CompanyConnection();
+    this.companyConnection.dbName = value.dbName;
+    this.companyConnection.host = value.host;
+    this.companyConnection.port = value.port;
+    this.companyConnection.username = value.username;
+    this.companyConnection.password = value.password;
+    this.companyConnection.dialect = value.dialect;
+    console.log(this.companyConnection);
+    this.companyConnectionService.testDBCompanyConnection(this.companyConnection).subscribe(
       (res) => {
-        this.toast.success("Update Account success!");
+        const status: any = res;
+        if (status.status == 0) {
+          this.toast.error("Connection fail!!");
+        } else if (status.status == 200) {
+          this.toast.success("Database connection success!");
+        }
+      },
+      (error) => {
+        this.toast.error("Server is not available!");
+      }
+    )
+  }
+
+  // save connection string
+  onSubmitConection(value) {
+    this.companyConnection = new CompanyConnection();
+    this.companyConnection.dbName = value.dbName;
+    this.companyConnection.host = value.host;
+    this.companyConnection.port = value.port;
+    this.companyConnection.username = value.username;
+    this.companyConnection.password = value.password;
+    this.companyConnection.dialect = value.dialect;
+    const connectionString =
+      this.companyConnection.dbName + " " +
+      this.companyConnection.host + " " +
+      this.companyConnection.port + " " +
+      this.companyConnection.username + " " +
+      this.companyConnection.password + " " +
+      this.companyConnection.dialect
+    console.log(connectionString);
+    this.companyServices.updateAccountCompany(connectionString).subscribe(
+      (res) => {
+        this.toast.success("Save connection success!");
       },
       (error) => {
         this.toast.error("Server is not available!");
@@ -124,30 +165,30 @@ export class CompanyConfigConnectionComponent implements OnInit {
   //dynamic
   mainForm() {
     this.getTime = this.fb.group({
-        time: this.fb.array([
-            this.addTimeForm()
-        ])
+      time: this.fb.array([
+        this.addTimeForm()
+      ])
     });
 
   }
 
   onAddTime() {
-      (<FormArray>this.getTime.controls['time']).push(this.addTimeForm());
+    (<FormArray>this.getTime.controls['time']).push(this.addTimeForm());
 
   }
 
   addTimeForm(): FormGroup {
-      this.timeForm = this.fb.group({
-          timeControl: ['', Validators.required],
-      });
-      return this.timeForm;
+    this.timeForm = this.fb.group({
+      timeControl: ['', Validators.required],
+    });
+    return this.timeForm;
   }
   removeUnit(i: number) {
-      const control = <FormArray>this.getTime.controls['time'];
-      control.removeAt(i);
+    const control = <FormArray>this.getTime.controls['time'];
+    control.removeAt(i);
   }
 
-  submit(){
+  submit() {
     let date = JSON.stringify(this.fileContent);
     console.log(date);
   }
@@ -156,7 +197,7 @@ export class CompanyConfigConnectionComponent implements OnInit {
     let file = fileList[0];
     let fileReader: FileReader = new FileReader();
     let self = this;
-    fileReader.onloadend = function() {
+    fileReader.onloadend = function () {
       self.fileContent = fileReader.result;
     }
     fileReader.readAsText(file);
---
+++ src/app/model/company_connection.ts
@@ -0,0 +1,8 @@
+export class CompanyConnection {
+    dbName: String
+    username: String
+    password: String
+    host: String
+    dialect: String
+    port: String
+}
