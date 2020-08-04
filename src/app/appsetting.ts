d658720b7bb2918569f4b8421b2cb8bddf5cd652
sonnguyen97 <nguyencongson163@gmail.com>
2020-08-04 13:46:49 +0700

    update

--- src/app/appsetting.ts
+++ src/app/appsetting.ts
@@ -1,10 +1,12 @@
 export class AppSettings {
+    // public static BASEURL = 'https://gmhrs-api.herokuapp.com/api/';
     public static BASEURL = 'http://103.143.209.237:3000/api/';
     // public static BASEURL = 'http://localhost:3000/api/';
     public static SCHEDULE = 'schedules/'
     public static EMP = 'employees/'
     public static TEAM = 'teams/'
     public static DEPARTMENT = 'departments/'
+    public static EMAIL = 'email/'
     public static TWOFAAUTHGETQRCODE = 'twofa/getQRCode/'
     public static CHECKOTP = 'twofa/checkotp/'
     public static ACTIVATED2FA = 'twofa/activated2fa/'
@@ -30,6 +32,9 @@ export class AppSettings {
     public static GETSIGNATURERULEBYID = 'signature/getSignatureRuleByID/'
     public static DELETETEMPLATERULEBYID = 'signature/deleteTemplateRuleByID/'
     public static GETDYNAMICRULE = 'signature/getDynamicRule/'
+    public static ACCOUNT = 'accounts/';
+    public static SIGNATURE = 'signature/';
+    public static FILE = 'file/';
     public static GETSPECIFICRULE = 'signature/getSpecificRule/'
     public static SAVESPECSIGNATURE = 'signature/saveSpecSignature/'
 }
